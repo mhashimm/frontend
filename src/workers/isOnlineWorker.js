c75c95842f711b4755898ce4e3782f3d8f975dfe
mhashim <mhashim.gm@gmail.com>
2016-03-18 09:22:12 +0300

    Make online worker check status immediately

--- src/workers/isOnlineWorker.js
+++ src/workers/isOnlineWorker.js
@@ -1,21 +1,24 @@
 import config from 'config'
 
 global.onmessage = function getOrg() {
-  setInterval(() => {
-    global.fetch(config.url, {
-      method: 'GET',
-      mode: config.cors,
-      headers: new Headers({
-        'pragma': 'no-cache',
-        'cache-control': 'no-cache'
-      })
+  checkOnlineStatus()
+  setInterval(checkOnlineStatus, config.onlineCheckInterval)
+}
+
+const checkOnlineStatus = () => {
+  global.fetch(config.url, {
+    method: 'GET',
+    mode: config.cors,
+    headers: new Headers({
+      'pragma': 'no-cache',
+      'cache-control': 'no-cache'
     })
-    .then(response => {
-      //TODO status "0" to handle local files fetching (e.g. Cordova/Phonegap etc.)
-      if(response.status === 200)
-        postMessage(true)
-      else postMessage(false)
-      })
-    .catch(() => postMessage(false))
-  }, config.onlineCheckInterval)
+  })
+  .then(response => {
+    //TODO status "0" to handle local files fetching (e.g. Cordova/Phonegap etc.)
+    if(response.status === 200)
+      postMessage(true)
+    else postMessage(false)
+    })
+  .catch(() => postMessage(false))
 }
