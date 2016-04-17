adcca4cc58cc4581dfd03447e08acc1a76873653
mhashim <mhashim.gm@gmail.com>
2016-04-17 10:38:36 +0300

    Wait for token before requesting org from orgWorker

--- src/workers/orgWorker.js
+++ src/workers/orgWorker.js
@@ -13,28 +13,33 @@ global.onmessage = function getOrg(event) {
     })
   }))
   .then(lst => {
-    fetch(config.apiUrl + config.admin.path.get +
-        `?facultyts=${lst[2]}&departmentts=${lst[1]}&coursets=${lst[0]}&programts=${lst[3]}`, {
-        method: 'GET',
-        mode: config.cors,
-        headers: new Headers({
-          'Authorization':  'Bearer ' + user.token
-        })
-      }).then(response => {
-        if(response.status == 200){
-          return response.json()
-        }
-      }).then(org => {
-        Object.keys(org).map(key =>{
-          org[key].map(entity =>{
-            const current = db[key], orig = db[key + 'Orig']
-            db.transaction('rw', current, orig, function() {
-              current.delete(entity.id)
-              current.add(entity)
-              orig.delete(entity.id)
+    let interval = setInterval(() => {
+      if(!!user.token){
+        clearInterval(interval)
+        fetch(config.apiUrl + config.admin.path.get +
+          `?facultyts=${lst[2]}&departmentts=${lst[1]}&coursets=${lst[0]}&programts=${lst[3]}`, {
+          method: 'GET',
+          mode: config.cors,
+          headers: new Headers({
+            'Authorization':  'Bearer ' + user.token
+          })
+        }).then(response => {
+          if(response.status == 200){
+            return response.json()
+          }
+        }).then(org => {
+          Object.keys(org).map(key =>{
+            org[key].map(entity =>{
+              const current = db[key], orig = db[key + 'Orig']
+              db.transaction('rw', current, orig, function() {
+                current.delete(entity.id)
+                current.add(entity)
+                orig.delete(entity.id)
+              })
             })
           })
         })
-      })
-    })
+      }
+    }, 1)
+  })
 }
